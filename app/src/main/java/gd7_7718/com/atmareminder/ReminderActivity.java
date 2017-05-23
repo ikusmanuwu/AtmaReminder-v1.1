7cd97f0bf244bb3d40db106c9e641461b01b5309
Unknown <ikusmanuwu@gmail.com>
2017-05-23 12:17:20 +0700

    Alarm Kurang cek jam di hari sama

--- app/src/main/java/gd7_7718/com/atmareminder/ReminderActivity.java
+++ app/src/main/java/gd7_7718/com/atmareminder/ReminderActivity.java
@@ -19,6 +19,7 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import java.text.ParseException;
 import java.util.Calendar;
 import java.util.Random;
 
@@ -70,18 +71,57 @@ public class ReminderActivity extends AppCompatActivity {
         // set the alarm to the time that you picked
         final Calendar calendar = Calendar.getInstance();
 
+        final int day = calendar.get(Calendar.DAY_OF_WEEK);
+
+        Log.e("day",day+"");
+
+        switch (day) {
+            case Calendar.SUNDAY:
+                // Current day is Sunday
+
+            case Calendar.MONDAY:
+                // Current day is Monday
+
+            case Calendar.TUESDAY:
+                // etc.
+            case Calendar.WEDNESDAY:
+            case Calendar.THURSDAY:
+            case Calendar.FRIDAY:
+            case Calendar.SATURDAY:
+        }
+
         alarmTimePicker = (TimePicker) findViewById(R.id.alarmTimePicker);
 
+
+
+
+
+        /*SQLiteDatabase db = helper.getReadableDatabase();
+
+        String table = "table2";
+        String[] columns = {"column1", "column3"};
+        String selection = "column3 =?";
+        String[] selectionArgs = {"apple"};
+        String groupBy = null;
+        String having = null;
+        String orderBy = "column3 DESC";
+        String limit = "10";
+
+        Cursor cursor = db.query(table, columns, selection, selectionArgs, groupBy, having, orderBy, limit);
 
         ///////////////////getDatabase/////////////////////////////////
 
         dbHelper = new DataHelper(this);
-       /*
+
         SQLiteDatabase db = dbHelper.getReadableDatabase();
-        cursor = db.rawQuery("SELECT TOP1 idhari FROM jadwal WHERE idhari = '" +
-                Calendar.DAY_OF_WEEK + "'",null);
+
+         cursor = db.rawQuery("SELECT  * FROM jadwal WHERE idhari = '" +
+                day + "'" + "AND jammulai",null);
         cursor.moveToFirst();
-        */
+*/
+
+
+
 
 
         ///////////////////////////////////////////////////////////////
@@ -97,19 +137,24 @@ public class ReminderActivity extends AppCompatActivity {
                 calendar.add(Calendar.SECOND, 3);
                 //setAlarmText("You clicked a button");
 
-                final int hour = alarmTimePicker.getCurrentHour();
-                final int minute = alarmTimePicker.getCurrentMinute();;
-                Log.e("MyActivity", "In the receiver with " + hour + " and " + minute);
+//                final int hour = alarmTimePicker.getCurrentHour();
+//                final int minute = alarmTimePicker.getCurrentMinute();
+
+                String string = getJammulai();
+                String[] parts = string.split(":", 2);
+                String jam = parts[0]; // 004
+
+                String menit = parts[1]; // 034556-42
+                final int hour = Integer.parseInt(jam);
+                final int minute = Integer.parseInt(menit);
+                Log.e("MyActivity", "In the receiver with " + (hour-1) + " and " + minute);
                 setAlarmText("You clicked a " + hour + " and " + minute);
 
-                if(Calendar.DAY_OF_WEEK==1)
-                {
-
-                }
-
-                calendar.set(Calendar.HOUR_OF_DAY, alarmTimePicker.getCurrentHour());
-                calendar.set(Calendar.MINUTE, alarmTimePicker.getCurrentMinute());
-//                calendar.set(Calendar.DAY_OF_WEEK,1);
+//                calendar.set(Calendar.HOUR_OF_DAY, alarmTimePicker.getCurrentHour());
+//                calendar.set(Calendar.MINUTE, alarmTimePicker.getCurrentMinute());
+                calendar.set(Calendar.MINUTE, minute);
+                calendar.set(Calendar.HOUR_OF_DAY,hour-1);
+                calendar.set(Calendar.DAY_OF_WEEK,day);
 
                 myIntent.putExtra("extra", "yes");
                 pending_intent = PendingIntent.getBroadcast(ReminderActivity.this, 0, myIntent, PendingIntent.FLAG_UPDATE_CURRENT);
@@ -148,6 +193,44 @@ public class ReminderActivity extends AppCompatActivity {
         });
 
     }
+
+    public String getJammulai(){
+
+        final Calendar calendar = Calendar.getInstance();
+
+        final int day = calendar.get(Calendar.DAY_OF_WEEK);
+
+        switch (day) {
+            case Calendar.SUNDAY:
+                // Current day is Sunday
+
+            case Calendar.MONDAY:
+                // Current day is Monday
+
+            case Calendar.TUESDAY:
+                // etc.
+            case Calendar.WEDNESDAY:
+            case Calendar.THURSDAY:
+            case Calendar.FRIDAY:
+            case Calendar.SATURDAY:
+        }
+
+
+        dbHelper = new DataHelper(this);
+
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        cursor = db.rawQuery("SELECT  * FROM jadwal WHERE idhari = " + day,null);
+
+        while (cursor.moveToNext()){
+
+            if (cursor.getInt(cursor.getColumnIndex("idhari"))== day)
+            return cursor.getString(cursor.getColumnIndex("jammulai"));
+
+        }
+        return "Tidak ditemukan";
+
+    }
+
 
     public void setAlarmText(String alarmText) {
         alarmTextView.setText(alarmText);
