115eddd8422aac6358453678a06352dead1b9da6
Unknown <davidkloning@gmail.com>
2017-05-30 03:05:29 +0700

    splash

---
+++ app/src/main/java/gd7_7718/com/atmareminder/Splash.java
@@ -0,0 +1,10 @@
+package gd7_7718.com.atmareminder.Entity;
+
+import com.viksaa.sssplash.lib.activity.AwesomeSplash;
+
+/**
+ * Created by Daaavid on 5/30/2017.
+ */
+
+public class Splash extends AwesomeSplash{
+}
